ed6e6218bf453ea914969b11d9536e48043e71a5
Matthew <bakebill20@yahoo.com>
2020-11-24 13:57:32 -0600

    working on delete function

--- JS/movieApp.js
+++ JS/movieApp.js
@@ -1,6 +1,39 @@
-console.log("movies");
 const baseURL = 'https://accidental-petite-cruiser.glitch.me/movies'
+$(document).ready(function() {
+    fetch(baseURL)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            $("#after").css("display", "block");
+            $("#load").fadeOut(300);
+        });
+});
 
-fetch(baseURL)
-.then(response => response.json())
-.then(response => console.log(response))
+
+//===== PUT => EDIT
+
+
+
+
+
+//======GET
+
+
+
+
+//=======DELETE
+const deleteMovie = (id) => fetch(`${baseURL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}).then(response => response.json()
+).then(() => {
+    console.log(`Success, deleted movie ${id}`);
+}).catch(error => console.log(error));
+
+// deleteMovie(1).then((data) => console.log(data));
+
+// ========= EDIT
+
+
