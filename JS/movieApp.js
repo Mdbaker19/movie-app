7c8c4240b740eb8de9d4104aa5e7a035424ab518
Matthew <bakebill20@yahoo.com>
2020-11-24 16:27:37 -0600

    added a render html, some css color and layout

--- JS/movieApp.js
+++ JS/movieApp.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    const baseURL = 'https://accidental-petite-cruiser.glitch.me/movies'
-    let currentMovie = []
+    const baseURL = 'https://accidental-petite-cruiser.glitch.me/movies';
+    let allMovies = [];
     let movie1 = {
         "title": "up",
         "rating": "5",
@@ -18,7 +18,6 @@ $(document).ready(function() {
 
 
 
-    //title, rating, genre
     function createMovie(i1, i2, i3){
         let newMovie = {
             title: i1,
@@ -28,9 +27,19 @@ $(document).ready(function() {
         return newMovie
     }
 
+    function render(data){
+        let html = `<div class="movieCard">`;
+        html += `<button class="delete">X</button>`;
+        html+=`<h1>${data.title}</h1>`;
+        html+=`<p>${data.rating}</p>`;
+        html+=`<p>${data.genre}</p>`;
+        html+=`</div>`;
+        return html
+    }
 
-
-
+$("#submit").on("click", function (){
+    addMovie(createMovie(title.val(), rating.val(), genre.val())).then(response => console.log(response));
+});
 
 
 //=====POST
@@ -43,11 +52,11 @@ $(document).ready(function() {
     })
         .then(res => res.json())
         .then(data => {
+            $(".movieSection").innerHTML += render(data);
             console.log(`Success: created ${JSON.stringify(data)}`);
             return data.id; // to access the primary key of the newly created entity
         })
         .catch(console.error);
-// addMovie(createMovie('Terminator','5 Stars', "Action")).then((response) => console.log(response));
 
 
 
@@ -76,13 +85,19 @@ $(document).ready(function() {
     fetch(baseURL)
         .then(response => response.json())
         .then(response => {
-            console.log(response);
-           currentMovie = response;
-            $("#after").css("display", "block");
+            $("#after").css("display", "flex");
             $("#load").fadeOut(300);
+            allMovies = response;
+            console.log(allMovies);
+            showMovies(allMovies);
         });
 
 
+function showMovies(arr){
+    for(let i = 0; i < arr.length; i++){
+        $(".movieSection")[0].innerHTML+= render(arr[i]);
+    }
+}
 
 
 //=======DELETE
@@ -96,7 +111,6 @@ const deleteMovie = (id) => fetch(`${baseURL}/${id}`, {
     console.log(`Success, deleted movie ${id}`);
 }).catch(error => console.log(error));
 
-// deleteMovie(1).then((data) => console.log(data));
 
 
 
