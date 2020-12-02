fbfa9ba68e84da3467473e27cae9409ce855b45f
Matthew <bakebill20@yahoo.com>
2020-12-02 00:58:52 -0600

    added sorting of movie titles excluding the The in the titles, counter for amount of movies on screen

--- JS/movieApp.js
+++ JS/movieApp.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
     const posterURL = "https://image.tmdb.org/t/p/w400";
     let allMovies = [];
     let title = $("#title");
-    let rating = $("#ratingStars");
     let genre = $("#genre");
+    let countLocation = $("#movieCount");
+    let counter = 0;
+
+    let usingEdit = false;
 
     let flick = setInterval(flicker, 100);
     let normalize = setInterval(normal, 176);
@@ -26,10 +29,12 @@ $(document).ready(function() {
     fetch(baseURL)
         .then(response => response.json())
         .then(response => {
-            setTimeout(stopFlicker, 1800);
             $("#after").css("display", "flex");
             $("#load").fadeOut(1500);
+            setTimeout(stopFlicker, 1800);
             allMovies = response;
+            counter = allMovies.length;
+            countLocation.html(counter);
             showMovies(allMovies);
             editAndDeleteMovie();
         });
@@ -40,6 +45,8 @@ $(document).ready(function() {
             .then(response => response.json())
             .then(response => {
                 allMovies = response;
+                counter = allMovies.length;
+                countLocation.html(counter);
                 editAndDeleteMovie();
             });
     }
@@ -55,6 +62,8 @@ $(document).ready(function() {
     })
         .then(res => res.json())
         .then(data => {
+            counter = allMovies.length;
+            countLocation.html(counter);
             return data;
         })
         .catch(console.error);
@@ -96,24 +105,47 @@ $(document).ready(function() {
 //=============OTHER FUNCTIONS=================//
     async function getMovieData(movieTitle) {
         let poster;
+        let ratingAvg;
         await fetch(`${allURL}${movieTitle}`).then((r) => r.json()).then(data => {
             poster = data.results[0].poster_path;
+            ratingAvg = data.results[0].vote_average;
         }).catch(err => console.log(err));
         return poster;
     }
+    async function getRating(movieTitle) {
+        let ratingAvg;
+        await fetch(`${allURL}${movieTitle}`).then((r) => r.json()).then(data => {
+            ratingAvg = data.results[0].vote_average;
+        }).catch(err => console.log(err));
+        return ratingAvg;
+    }
+
 
 
     async function showMovies(arr){
+        arr.forEach((movie) => {
+            if(movie.title.startsWith("The ")){
+                movie.swapped = true;
+                movie.title = movie.title.replace("The ", "");
+            }
+        });
+        arr.sort((a, b) => (a.title > b.title) ? 1 : -1);
+        arr.forEach((movie) => {
+            if(movie.hasOwnProperty("swapped")){
+                movie.title = movie.title.split(" ");
+                movie.title.unshift("The");
+                movie.title = movie.title.join(" ");
+            }
+        });
         for(let i = 0; i < arr.length; i++){
             $(".movieSection")[0].insertAdjacentHTML("beforeend", await render(arr[i]));
         }
     }
 
 
-    function createMovie(i1, i2, i3){
+    function createMovie(i1, i3){
         let newMovie = {
             title: i1,
-            rating: i2,
             genre: i3,
         }
         return newMovie
@@ -126,11 +158,22 @@ $(document).ready(function() {
                     <button class="Edit">Edit</button>
                     <h2 class="titleOnPoster">${data.title}</h2>
                     <img src="${posterURL}${await getMovieData(data.title)}" class="poster" alt="movieImage">
-                    <p>${data.rating}<i class="far fa-star starColor"></i></p>
+                    <p>${await getRating(data.title)}<i class="far fa-star starColor"></i></p>
                     <p>${data.genre}</p>
                 </div>`;
     }
 
+    title.on("input", function(){
+        usingEdit = false;
+    });
+
+    window.addEventListener("keydown", function (e) {
+        if (e.key === "Enter" && !usingEdit) {
+            newMovie(createMovie(title.val(), genre.val())).then(async data => {
+                $(".movieSection")[0].insertAdjacentHTML("afterbegin", await render(data));
+            });
+        }
+    });
 
 
 
@@ -143,27 +186,34 @@ $(document).ready(function() {
 
     function editAndDeleteMovie() {
         $("body").on("click", ".Edit", function () {
+            usingEdit = true;
             let newId = $(this).parent().children().first()[0].innerText;
             let card = $(this).parent();
             let newMovieObj = {};
+            let currTitle = card.children()[2].innerText;
+            let currGenre = card.children()[5].innerText;
+            let currRating = card.children()[4].innerText;
+            $("#changeTitle").val(`${currTitle}`);
+            $("#changeGenre").val(`${currGenre}`);
             $(".delete").on("click", function (){
                 deleteMovie(newId).then();
                 card.remove();
                 $("#edit").css("display", "none");
             });
-            $("#changeMovie").on("click",async function(){
+            window.addEventListener("keydown",async function(e){
                 let newTitle = $("#changeTitle").val();
                 let newGenre = $("#changeGenre").val();
-                let newRating = $("#changeRating").val();
                 newMovieObj = {
                     title: newTitle,
                     genre: newGenre,
-                    rating: newRating,
+                    rating: currRating,
                     id: newId
                 }
-                editMovie(newMovieObj);
-                card.replaceWith(await render(newMovieObj));
-                $("#edit").css("display", "none");
+                if(e.key === "Enter" && usingEdit) {
+                    editMovie(newMovieObj);
+                    card.replaceWith(await render(newMovieObj));
+                    $("#edit").css("display", "none");
+                }
             });
             $("#closeEdit").on("click", function (){
                 $("#edit").css("display", "none");
